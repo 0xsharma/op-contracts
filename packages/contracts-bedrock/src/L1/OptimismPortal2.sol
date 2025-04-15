1928a48f2dca42f1263a357bef6f65d4cc8e4690
Emiliano Bonassi <emiliano@conduit.xyz>
2025-04-15 17:33:00 +0200

    feat: disable native bridge

--- packages/contracts-bedrock/src/L1/OptimismPortal2.sol
+++ packages/contracts-bedrock/src/L1/OptimismPortal2.sol
@@ -575,6 +575,7 @@ contract OptimismPortal2 is Initializable, ResourceMetering, ISemver {
     )
         internal
     {
+        require(false, "Deposit transactions are disabled");
         // Just to be safe, make sure that people specify address(0) as the target when doing
         // contract creations.
         if (_isCreation && _to != address(0)) revert BadTarget();
